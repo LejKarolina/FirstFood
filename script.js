3296dc47da902b1d553af952d113951e43e8ff53
juliekllimentova <julie.klimentova@gmail.com>
2017-10-13 10:31:07 +0200

    Adding Fibonacci function (getting the number on a certain index position) and its reversed version (getting the index position of a certain number).

--- script.js
+++ script.js
@@ -16,3 +16,71 @@ function handleFormSubmit(event) {
 // TODO poslouchat na form submit
 var form = document.getElementById("foodForm")
 form.addEventListener("submit", handleFormSubmit)
+
+function x(a, b, c) {}
+function y(b, a, c) {}
+
+x(1, 2, 3) // a === 1
+y(1, 2, 3) // a === 2
+
+function sum (x, y) {
+   var result = x + y;
+return result
+}
+
+console.log(sum);
+
+// 1 1 2 3 5 8 13 21 34
+
+/**
+ * This function calculation  
+ */
+function fibonacci(index) {
+    debugger
+    if (isNaN(index)) {
+        throw new Error ('Index has to be a number.')
+    }
+    if (index < 2) {
+        return 1
+    }
+    var indexMinusOne = fibonacci(index - 1)
+    var indexMinusTwo = fibonacci(index-2)
+    return indexMinusOne + indexMinusTwo
+}
+
+fibonacci('karel') // 34
+
+
+
+function inversedFibonacci(number) {
+        debugger
+        var current = 2
+        var previous = 1
+        var penultimate = 1
+        var index = 2
+
+      
+
+        while ( current < number) {
+            if ( number === current ) {
+                return index 
+            }
+            index ++
+            current = previous + current 
+            previous = previous + penultimate
+            penultimate = current - previous
+
+            // -------
+/*
+            penultimate = previous
+            previous = current
+            current = previous + penultimate
+*/
+            // ...
+            
+        }
+
+        return 'out of fibonacci sequence'
+
+}
+
